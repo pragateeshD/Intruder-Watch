496263382c036fc94b97554195ffab840bc244aa
pragateesh dharmaraj <pragateeshdr@gmail.com>
2024-07-13 16:07:03 +0530

    Update App.test.js

--- Intruder-Watch/demo/src/App.test.js
+++ Intruder-Watch/demo/src/App.test.js
@@ -1,4 +1,4 @@
- import { render, screen } from '@testing-library/react';
+ impoort { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
