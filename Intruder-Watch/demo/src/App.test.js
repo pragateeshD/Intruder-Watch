6d2d01100252c11369a60e93d7d57d54b58ecc06
pragateesh dharmaraj <pragateeshdr@gmail.com>
2024-07-17 15:30:31 +0530

    Update App.test.js

--- Intruder-Watch/demo/src/App.test.js
+++ Intruder-Watch/demo/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn reactlink', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.get ByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
